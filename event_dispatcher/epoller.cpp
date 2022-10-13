774443eaca037b637fe8c57c6bc275e44c3811ef
kkk <82047973+kkk-455123@users.noreply.github.com>
2022-10-13 17:56:00 +0800

    Add files via upload

--- event_dispatcher/epoller.cpp
+++ event_dispatcher/epoller.cpp
@@ -1,13 +1,13 @@
 #include "epoller.h"
 
 Epoller::Epoller(int Maxevent)
-	: epollFd(epoll_create(1))
-	, events(Maxevent) {  // events将包含Maxevent个元素，每个元素进行缺省的值初始化。为当程序运行初期元素大致数量可预知，而元素的值需要动态获取的时候，可采用这种初始化方式
-	assert(epollFd > 0 && events.size() > 0);
+	: m_epollFd(epoll_create(1))
+	, m_events(Maxevent) {  // events将包含Maxevent个元素，每个元素进行缺省的值初始化。为当程序运行初期元素大致数量可预知，而元素的值需要动态获取的时候，可采用这种初始化方式
+	assert(m_epollFd > 0 && m_events.size() > 0);
 }
 
 Epoller::~Epoller() {
-	close(epollFd);
+	close(m_epollFd);
 }
 
 bool
@@ -18,18 +18,16 @@ Epoller::AddFd(int fd, uint32_t events) {
 	event.data.fd = fd;
 	event.events = events;
 
-	return epoll_ctl(epollFd, EPOLL_CTL_ADD, fd, &event);
+	return epoll_ctl(m_epollFd, EPOLL_CTL_ADD, fd, &event);
 }
 
 bool
-Epoller::DelFd(int fd, uint32_t events) {
+Epoller::DelFd(int fd) {
 	if (fd < 0) return false;
 
 	epoll_event event{NULL};
-	event.data.fd = fd;
-	event.events = events;
 
-	return epoll_ctl(epollFd, EPOLL_CTL_DEL, fd, &event);
+	return epoll_ctl(m_epollFd, EPOLL_CTL_DEL, fd, &event);
 }
 
 bool
@@ -40,5 +38,21 @@ Epoller::ModFd(int fd, uint32_t events) {
 	event.data.fd = fd;
 	event.events = events;
 
-	return epoll_ctl(epollFd, EPOLL_CTL_MOD, fd, &event);
+	return epoll_ctl(m_epollFd, EPOLL_CTL_MOD, fd, &event);
+}
+
+int 
+Epoller::Wait(int timeoutMs) {
+	return epoll_wait(m_epollFd, m_events.data(), static_cast<int>(m_events.size()), timeoutMs);
+}	// data()返回vector数组首元素地址
+
+int
+Epoller::GetEventFd(size_t i) const {
+	assert(i >= 0 && i < m_events.size());
+	return m_events[i].data.fd;
+}
+
+uint32_t Epoller::GetEvent(size_t i) const {
+	assert(i >= 0 && i < m_events.size());
+	return m_events[i].events;
 }
--- event_dispatcher/epoller.h
+++ event_dispatcher/epoller.h
@@ -13,16 +13,20 @@ public:
 	// 向epoll实例注册文件描述符fd需要被监控的事件
 	bool AddFd(int fd, uint32_t events);
 	// 在epoll实例中删除文件描述符fd对应的事件
-	bool DelFd(int fd, uint32_t events);
+	bool DelFd(int fd);
 	// 修改epoll实例中文件描述符fd对应的事件
 	bool ModFd(int fd, uint32_t events);
 
 	// 等待内核I/O事件分发
 	int Wait(int timeoutMs = -1);
+	// 获取发生事件的fd
+	int GetEventFd(size_t i) const;
+	// 获取具体事件
+	uint32_t GetEvent(size_t i) const;
 
 private:
-	int epollFd;  // epoll实例，用来调用epoll_ctl和epoll_wait
+	int m_epollFd;  // epoll实例，用来调用epoll_ctl和epoll_wait
 	// 服务器关闭后需要调用close()方法释放epoll实例
 
-	std::vector<struct epoll_event> events;  // 元素为一个结构体，表示注册的事件类型，其中最常用的是fd字段，表示事件对应的文件描述符
+	std::vector<struct epoll_event> m_events;  // 元素为一个结构体，表示注册的事件类型，其中最常用的是fd字段，表示事件对应的文件描述符
 };
