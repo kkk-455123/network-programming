2b3e9bc2406e22317e6b87a10dcaebf05379a682
kkk <82047973+kkk-455123@users.noreply.github.com>
2022-10-18 00:12:18 +0800

    little modify2

--- epoll_server.cpp
+++ epoll_server.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "./network-programming/connect-class/TCP.h"
-#include "./network-programming/event_dispatcher/epoller.h"
+#include "./connect-class/TCP.h"
+#include "./event_dispatcher/epoller.h"
 
 char CharChange(char c) {
 	if (c >= 'a' && c < 'h') return 'a';
@@ -22,6 +22,7 @@ int main() {
 		std::cout << "epoll_wait wakeup" << std::endl;
 		for (size_t i = 0; i < n; i++)
 		{
+			std::cout << "start processing event:" << i << std::endl;
 			int event_fd = epoller.GetEventFd(i);
 			uint32_t event_event = epoller.GetEvent(i);
 			// error事件
@@ -46,29 +47,29 @@ int main() {
 				int socket_fd = event_fd;
 				std::cout << "get event on socket fd == " << socket << std::endl;
 				// 接收数据
-				//while (1) {
-				//	char buf[512];
-				//	// 读数据出错
-				//	if ((n = read(socket_fd, buf, sizeof(buf))) < 0) {
-				//		if (errno != EAGAIN) {
-				//			perror("read error");
-				//			close(socket_fd);
-				//		}
-				//		break;
-				//	}
-				//	// 客户端正常关闭, 关闭连接套接字 
-				//	else if (n == 0) {
-				//		close(socket_fd);
-				//		break;
-				//	}
-				//	// 数据处理逻辑and发送数据
-				//	else {
-				//		for (int i = 0; i < n; i++) {
-				//			buf[i] = CharChange(buf[i]);
-				//		}
-				//		if (write(socket_fd, buf, n) < 0) perror("write error");
-				//	}
-				//}
+				while (1) {
+					char buf[512];
+					int n_read = read(socket_fd, buf, sizeof(buf));
+					// 读数据出错
+					if (n_read < 0) {
+						if (errno != EAGAIN) {
+							perror("read error");
+							close(socket_fd);
+						}
+						break;
+					}
+					// 客户端正常关闭, 关闭连接套接字 
+					else if (n_read == 0) {
+						std::cout << "close normally." << std::endl;
+						close(socket_fd);
+						break;
+					}
+					// 数据处理逻辑and发送数据
+					else {
+						if (write(socket_fd, buf, n_read) < 0) perror("write error");
+						std::cout << "send finish." << std::endl;
+					}
+				}
 			}
 			
 		}
